8dab9230496f3ba924ceebc05f5b79b20b7c4036
iambenji-44 <adjabengtwumbenjamin@gmail.com>
2025-05-08 22:56:29 +0000

    add a Header Section

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //Etch-A-Sketch
+
+
+//HEADER
+let header = document.querySelector('#header');
+header.style.textAlign = 'center';
+header.style.padding = '20px';
+header.style.margin = '40px';
+
+let title = document.createElement('h1');
+title.classList.add('title')
+title.textContent = 'Etch-A-Sketch';
+
+
+let gridBtn = document.createElement('button');
+gridBtn.classList.add('gridBtn');
+gridBtn.textContent = 'Set Grid'
+gridBtn.style.width = '100px';
+gridBtn.style.margin = '15px';
+
+
+let resetBtn = document.createElement('button');
+resetBtn.classList.add('resetBtn');
+resetBtn.textContent = 'Reset'
+resetBtn.style.width = '100px';
+resetBtn.style.margin = '15px';
+
+
+header.appendChild(title);
+header.appendChild(gridBtn);
+header.appendChild(resetBtn);
